38163915d8e49d311bd5b81fdd95bdd71e4f0aba
GaiaGalimberti <gaiagali2006@gmail.com>
2026-05-22 21:22:39 +0200

    Add files via upload

---
+++ src/cinemax/model/menuProiezionista.java
@@ -0,0 +1,43 @@
+package src.cinemax.model;
+
+import src.cinemax.model.Proiezione;
+
+ public class menuProiezionista {
+  
+  
+  //CAMPI
+ 
+ private Proiezione[] = proiezioni;
+  private int numeroProiezioni;
+  
+  
+  //COSTRUTTORE
+  
+  public menuProiezionista() {
+   proiezioni = new Proiezione[10]; //l'array memorizza al massimo 10 proiezioni
+   numeroProiezioni = 0;
+   }
+   
+   
+   //METODI
+   
+   //aggiunge una proiezione
+   public void aggiungiProiezione(Proiezione p) {
+    proiezioni[numeroProiezioni] = p;
+	numeroProiezioni++;
+	}
+	
+   //visualizza tutte le proiezioni
+   public String visualizzaProiezioni() {
+    for(int i=0; i<numeroProiezioni; i++) {
+	 System.out.println("ecco l'elenco delle proiezioni: ");
+	 System.out.println(proiezioni[i]);
+	 }
+   }
+   
+   //modifica il prezzo di una proiezione
+   public void modificaPrezzo(int i, double nuovoPrezzo) {
+    proiezioni[i].setPrezzoBiglietto(nuovoPrezzo);
+	}
+  }
+   
